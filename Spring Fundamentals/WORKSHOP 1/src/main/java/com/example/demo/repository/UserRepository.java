02d40e9dfeee08473727374a6f78c64914feed8f
asennesa <angeorg117@gmail.com>
2020-06-03 13:54:59 +0300

    Done.

--- Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/repository/UserRepository.java
+++ Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.entity.User;
+import com.example.demo.model.service.UserServiceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
 
 }
---
+++ Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.entity.User;
+import com.example.demo.model.service.UserServiceModel;
+import com.example.demo.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleService roleService, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.roleService = roleService;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public UserServiceModel registerUser(UserServiceModel userServiceModel) {
+        userServiceModel.setRole(this.roleService.findByName(this.userRepository.count() == 0 ? "ADMIN" : "USER"));
+        User user = this.modelMapper.map(userServiceModel, User.class);
+
+        return this.modelMapper.map(this.userRepository.saveAndFlush(user), UserServiceModel.class);
+    }
+}
