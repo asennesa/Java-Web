80a28a0cc5e8fc6df38cf3a7eb96ac69660c1229
asennesa <angeorg117@gmail.com>
2020-06-17 16:35:33 +0300

    So far...

--- Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/service/UserService.java
+++ Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/service/UserService.java
@@ -2,8 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.model.service.UserServiceModel;
 
+import java.util.List;
+
 public interface UserService {
     UserServiceModel registerUser(UserServiceModel userServiceModel);
 
     UserServiceModel findByUsername(String username);
+
+    List<String> findAllUsernames();
+
+    void addRoleToUser(String username, String role)    ;
 }
--- Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ Spring Fundamentals/WORKSHOP 1/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.model.entity.Role;
 import com.example.demo.model.entity.User;
 import com.example.demo.model.service.UserServiceModel;
 import com.example.demo.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -33,5 +37,23 @@ public class UserServiceImpl implements UserService {
         return this.userRepository.findByUsername(username)
                 .map(user -> this.modelMapper.map(user, UserServiceModel.class))
                 .orElse(null);
+    }
+
+    @Override
+    public List<String> findAllUsernames() {
+        return this.userRepository.findAll()
+                .stream().map(user -> user.getUsername()).collect(Collectors.toList());
+    }
+
+    @Override
+    public void addRoleToUser(String username, String role) {
+        User user = this.userRepository.findByUsername(username).orElse(null);
+        if (!user.getRole().getName().equals(role)) {
+            Role roleEntity = this.modelMapper.map(this.roleService.findByName(role), Role.class);
+
+            user.setRole(roleEntity);
+            this.userRepository.saveAndFlush(user);
+
+        }
     }
 }
